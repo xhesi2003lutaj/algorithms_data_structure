34eeb773bc5d50e5ca87a53b36e7a35536e5d1e6
Perdorues <perdorues@lutajxhesilda.local>
2023-06-06 12:59:15 +0200

    x

---
+++ max_subarray.cpp
@@ -0,0 +1,38 @@
+
+#include <iostream>
+#include<algorithm>
+using namespace std;
+
+int maxSubArraySum(int a[], int size,int &index)
+{
+	int max_so_far = INT_MIN, max_ending_here = 0;
+
+	for (int i = 0; i < size; i++) {
+		max_ending_here = max_ending_here + a[i];
+		if (max_so_far < max_ending_here)
+        {
+            max_so_far = max_ending_here;
+            index=i;
+        }
+
+		if (max_ending_here < 0){
+            max_ending_here = 0;
+        }
+			
+	}
+    
+	return max_so_far;
+}
+
+int main()
+{
+	int a[] = { -2, -3, 4, -1, -2, 1, 5, -3 };
+	int n = sizeof(a) / sizeof(a[0]);
+
+    int index;
+	int max_sum = maxSubArraySum(a, n,index);
+   
+	cout << "Maximum contiguous sum is " << max_sum<<endl;
+    cout<<"The index here the max subarray ends is"<<index<<endl;
+	return 0;
+}
